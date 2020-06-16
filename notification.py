11b367bb4dfe7e4da0bef27bf3b9bd0eb7b08eb1
Spencer Albrecht <salbrecht13@bloomberg.net>
2020-06-15 21:51:14 -0400

    Change to paid number

--- notification.py
+++ notification.py
@@ -5,5 +5,5 @@ from twilio.rest import Client
 def send_alert_message(chance_of_rain):
     client = Client(os.environ['TWILIO_ACCOUNT_SID'], os.environ['TWILIO_AUTH_TOKEN'])
     message_body = f"Hello this is RainBot! There is a {chance_of_rain*100}% chance of rain today. Make sure you're prepared!"
-    client.messages.create(body=message_body,from_='+12053464547',to='+19092476022')
-    client.messages.create(body=message_body,from_='+12053464547',to='+17143154382')
+    client.messages.create(body=message_body,from_='+12058431429',to='+19092476022')
+    client.messages.create(body=message_body,from_='+12058431429',to='+17143154382')
